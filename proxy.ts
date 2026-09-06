02982f906fc39aa3991b5f24636b9065c5e0bd5e
Ken ken <95589932+Kesjs@users.noreply.github.com>
2026-09-06 09:32:12 +0100

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -19,7 +19,7 @@ export async function proxy(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options?: CookieOptions }[]) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
           response = NextResponse.next({ request: { headers: request.headers } });
           cookiesToSet.forEach(({ name, value, options }) =>
